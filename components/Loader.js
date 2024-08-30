7271bde35ce2362e4211eb5ca02d482ab11bde3b
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-08-30 10:06:30 +0530

    Add files via upload

---
+++ components/Loader.js
@@ -0,0 +1,39 @@
+// components/Loader.js
+
+import React from 'react';
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={styles.box}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={styles.box}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={styles.box}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+
+import Loader from '../components/Loader';
+
+export default function Home() {
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
+}
